566bc73e5fd03526096923e551f038a599744b72
Herman <cool.goga501@gmail.com>
2024-05-09 00:00:23 +0200

    feat/create statistic-client

--- statistic/statistic-client/src/main/java/ru/practicum/StatisticClient.java
+++ statistic/statistic-client/src/main/java/ru/practicum/StatisticClient.java
@@ -1,12 +1,47 @@
 package ru.practicum;
 
-import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.dto.CreateStatisticDto;
 
-public class StatisticClient {
+import java.util.List;
+import java.util.Map;
 
-    private final RestTemplate restTemplate;
+public class StatisticClient extends BaseClient {
 
-    public StatisticClient(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    private static final String API_PREFIX_HIT = "/hit";
+
+    private static final String API_PREFIX_STATS = "/stats";
+
+    private static final String SERVER_URL = "http://localhost:9090";
+
+
+    public StatisticClient(RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(SERVER_URL))
+                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                        .build()
+        );
+    }
+
+    public ResponseEntity<Object> getStatistic(
+            String start, String end, List<String> uris, boolean unique
+    ) {
+        String path = API_PREFIX_STATS + "?start={start}&end={end}&uris={uris}&unique={unique}";
+        Map<String, Object> parameters = Map.of(
+                "start", start,
+                "end", end,
+                "uris", uris,
+                "unique", unique
+        );
+
+        return get(path, parameters);
+    }
+
+    public ResponseEntity<Object> saveStatistic(CreateStatisticDto requestDto) {
+        return post(API_PREFIX_HIT, requestDto);
     }
 }

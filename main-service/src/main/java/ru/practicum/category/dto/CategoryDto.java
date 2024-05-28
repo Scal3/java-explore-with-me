c4153b9ff441e9a7d0bb90eeb72df836aa25552a
Herman <cool.goga501@gmail.com>
2024-05-29 01:31:02 +0200

    feat/create dto for category module

---
+++ main-service/src/main/java/ru/practicum/category/dto/CategoryDto.java
@@ -0,0 +1,15 @@
+package ru.practicum.category.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDto {
+
+    private Long id;
+
+    private String name;
+}
---
+++ main-service/src/main/java/ru/practicum/category/dto/GetCategoriesDto.java
@@ -0,0 +1,13 @@
+package ru.practicum.category.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GetCategoriesDto {
+
+    private int from;
+
+    private int size;
+}

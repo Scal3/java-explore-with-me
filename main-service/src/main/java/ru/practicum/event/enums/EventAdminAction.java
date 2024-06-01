f6749a0b91fd19b45a61f8fddfdf23e99e075c84
Herman <cool.goga501@gmail.com>
2024-06-01 23:07:22 +0200

    feat/create enums for event actions

---
+++ main-service/src/main/java/ru/practicum/event/enums/EventAdminAction.java
@@ -0,0 +1,8 @@
+package ru.practicum.event.enums;
+
+public enum EventAdminAction {
+
+    PUBLISH_EVENT,
+
+    REJECT_EVENT
+}
---
+++ main-service/src/main/java/ru/practicum/event/enums/EventUserAction.java
@@ -0,0 +1,8 @@
+package ru.practicum.event.enums;
+
+public enum EventUserAction {
+
+    SEND_TO_REVIEW,
+
+    CANCEL_REVIEW
+}
